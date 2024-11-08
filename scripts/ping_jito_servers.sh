a81d6e85f01712c96504f090f80c646233123f18
Ken Der <kenneth.der@gmail.com>
2024-11-08 05:14:36 -0700

    ping_jito_servers.sh

---
+++ scripts/ping_jito_servers.sh
@@ -0,0 +1,7 @@
+ping -c 5 mainnet.block-engine.jito.wtf
+ping -c 5 amsterdam.mainnet.block-engine.jito.wtf
+ping -c 5 frankfurt.mainnet.block-engine.jito.wtf
+ping -c 5 ny.mainnet.block-engine.jito.wtf
+ping -c 5 tokyo.mainnet.block-engine.jito.wtf
+ping -c 5 slc.mainnet.block-engine.jito.wtf
+
