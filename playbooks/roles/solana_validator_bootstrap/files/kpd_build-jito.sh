39fb7e8464fbdffe7975405d8a6c363f2e3b9ca0
Ken Der <kenneth.der@gmail.com>
2025-01-11 14:29:55 -0700

    playbooks/roles/solana_validator_bootstrap/files/kpd_build-jito.sh

--- playbooks/roles/solana_validator_bootstrap/files/kpd_build-jito.sh
+++ playbooks/roles/solana_validator_bootstrap/files/kpd_build-jito.sh
@@ -49,7 +49,7 @@ if git tag --list | grep "$TAG"; then
   CI_COMMIT=$(git rev-parse HEAD) scripts/cargo-install-all.sh --validator-only ~/.local/share/solana/install/releases/"$TAG"
   echo "updating symlinks for active_release..."
   rm -rf "$HOME"/.local/share/solana/install/active_release
-  ln -sf /home/solana/.local/share/solana/install/releases/"$TAG" "$HOME"/.local/share/solana/install/active_release
+  ln -sf "$HOME"/.local/share/solana/install/releases/"$TAG" "$HOME"/.local/share/solana/install/active_release
 else
   echo "invalid git tag: $TAG  hint: git tag|grep jito|sort -V"
 fi
